91d0f5faed3635cc8724d4725f45cc3d2a5d9437
Siarhei Dudko <slavianich@gmail.com>
2025-09-26 15:10:23 +0400

    fixes

--- test/library.test.ts
+++ test/library.test.ts
@@ -32,12 +32,8 @@ describe("Testing of all API methods.", function () {
       testTemplateArgs,
       testTemplateSize
     );
-    assert.equal(data instanceof ArrayBuffer, true);
-    const uint8Array = new Uint8Array(data);
-    assert.equal(uint8Array.includes(0x4a), true); // Check for 'J' from JFIF
-    assert.equal(uint8Array.includes(0x46), true); // Check for 'F' from JFIF
-    assert.equal(uint8Array.includes(0x49), true); // Check for 'I' from JFIF
-    assert.equal(uint8Array.includes(0x46), true); // Check for 'F' from JFIF
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("JFIF"), true);
   });
 
   it("createJpgFromPublicTemplate", async function () {
@@ -46,12 +42,8 @@ describe("Testing of all API methods.", function () {
       testTemplateArgs,
       testTemplateSize
     );
-    assert.equal(data instanceof ArrayBuffer, true);
-    const uint8Array = new Uint8Array(data);
-    assert.equal(uint8Array.includes(0x4a), true); // Check for 'J' from JFIF
-    assert.equal(uint8Array.includes(0x46), true); // Check for 'F' from JFIF
-    assert.equal(uint8Array.includes(0x49), true); // Check for 'I' from JFIF
-    assert.equal(uint8Array.includes(0x46), true); // Check for 'F' from JFIF
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("JFIF"), true);
   });
 
   it("createPdfFromPrivateTemplate", async function () {
@@ -60,11 +52,8 @@ describe("Testing of all API methods.", function () {
       testTemplateArgs,
       testTemplateSize
     );
-    assert.equal(data instanceof ArrayBuffer, true);
-    const uint8Array = new Uint8Array(data);
-    assert.equal(uint8Array.includes(0x50), true); // Check for 'P' from PDF
-    assert.equal(uint8Array.includes(0x44), true); // Check for 'D' from PDF
-    assert.equal(uint8Array.includes(0x46), true); // Check for 'F' from PDF
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("PDF"), true);
   });
 
   it("createPdfFromPublicTemplate", async function () {
@@ -73,11 +62,8 @@ describe("Testing of all API methods.", function () {
       testTemplateArgs,
       testTemplateSize
     );
-    assert.equal(data instanceof ArrayBuffer, true);
-    const uint8Array = new Uint8Array(data);
-    assert.equal(uint8Array.includes(0x50), true); // Check for 'P' from PDF
-    assert.equal(uint8Array.includes(0x44), true); // Check for 'D' from PDF
-    assert.equal(uint8Array.includes(0x46), true); // Check for 'F' from PDF
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("PDF"), true);
   });
 
   it("createPngFromPrivateTemplate", async function () {
@@ -86,11 +72,8 @@ describe("Testing of all API methods.", function () {
       testTemplateArgs,
       testTemplateSize
     );
-    assert.equal(data instanceof ArrayBuffer, true);
-    const uint8Array = new Uint8Array(data);
-    assert.equal(uint8Array.includes(0x50), true); // Check for 'P' from PNG
-    assert.equal(uint8Array.includes(0x4e), true); // Check for 'N' from PNG
-    assert.equal(uint8Array.includes(0x47), true); // Check for 'G' from PNG
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("PNG"), true);
   });
 
   it("createPngFromPublicTemplate", async function () {
@@ -99,11 +82,8 @@ describe("Testing of all API methods.", function () {
       testTemplateArgs,
       testTemplateSize
     );
-    assert.equal(data instanceof ArrayBuffer, true);
-    const uint8Array = new Uint8Array(data);
-    assert.equal(uint8Array.includes(0x50), true); // Check for 'P' from PNG
-    assert.equal(uint8Array.includes(0x4e), true); // Check for 'N' from PNG
-    assert.equal(uint8Array.includes(0x47), true); // Check for 'G' from PNG
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("PNG"), true);
   });
 
   it("createWebpFromPrivateTemplate", async function () {
@@ -112,12 +92,8 @@ describe("Testing of all API methods.", function () {
       testTemplateArgs,
       testTemplateSize
     );
-    assert.equal(data instanceof ArrayBuffer, true);
-    const uint8Array = new Uint8Array(data);
-    assert.equal(uint8Array.includes(0x57), true); // Check for 'W' from WEBP
-    assert.equal(uint8Array.includes(0x45), true); // Check for 'E' from WEBP
-    assert.equal(uint8Array.includes(0x42), true); // Check for 'B' from WEBP
-    assert.equal(uint8Array.includes(0x50), true); // Check for 'P' from WEBP
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("WEBP"), true);
   });
 
   it("createWebpFromPublicTemplate", async function () {
@@ -126,12 +102,118 @@ describe("Testing of all API methods.", function () {
       testTemplateArgs,
       testTemplateSize
     );
-    assert.equal(data instanceof ArrayBuffer, true);
-    const uint8Array = new Uint8Array(data);
-    assert.equal(uint8Array.includes(0x57), true); // Check for 'W' from WEBP
-    assert.equal(uint8Array.includes(0x45), true); // Check for 'E' from WEBP
-    assert.equal(uint8Array.includes(0x42), true); // Check for 'B' from WEBP
-    assert.equal(uint8Array.includes(0x50), true); // Check for 'P' from WEBP
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("WEBP"), true);
+  });
+});
+
+describe("Testing errors.", function () {
+  const irp = new IReceiptPRO("test");
+  const testTemplateId = "check";
+  const testTemplateArgs = { ping: "pong" };
+  const testTemplateSize = { width: 100, height: 210 };
+
+  it("Token invalid", async function () {
+    let error: Error | undefined;
+    try {
+      await irp.createWebpFromPublicTemplate(
+        testTemplateId,
+        testTemplateArgs,
+        testTemplateSize
+      );
+    } catch (err) {
+      error = err as Error;
+    }
+    assert.equal(error instanceof Error, true);
+    assert.equal(typeof error!.message, "string");
+    assert.equal(error!.message, "Token invalid");
+  });
+});
+
+describe("Testing of all API methods.", function () {
+  const irp = new IReceiptPRO(process.env.IRECEIPTPRO_API_KEY || "");
+  const testTemplateId = "check";
+  const testTemplateArgs = { ping: "pong" };
+  const testTemplateSize = { width: 100, height: 210 };
+
+  it("createJpgFromPrivateTemplate", async function () {
+    const data = await irp.createJpgFromPrivateTemplate(
+      testTemplateId,
+      testTemplateArgs,
+      testTemplateSize
+    );
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("JFIF"), true);
+  });
+
+  it("createJpgFromPublicTemplate", async function () {
+    const data = await irp.createJpgFromPublicTemplate(
+      testTemplateId,
+      testTemplateArgs,
+      testTemplateSize
+    );
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("JFIF"), true);
+  });
+
+  it("createPdfFromPrivateTemplate", async function () {
+    const data = await irp.createPdfFromPrivateTemplate(
+      testTemplateId,
+      testTemplateArgs,
+      testTemplateSize
+    );
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("PDF"), true);
+  });
+
+  it("createPdfFromPublicTemplate", async function () {
+    const data = await irp.createPdfFromPublicTemplate(
+      testTemplateId,
+      testTemplateArgs,
+      testTemplateSize
+    );
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("PDF"), true);
+  });
+
+  it("createPngFromPrivateTemplate", async function () {
+    const data = await irp.createPngFromPrivateTemplate(
+      testTemplateId,
+      testTemplateArgs,
+      testTemplateSize
+    );
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("PNG"), true);
+  });
+
+  it("createPngFromPublicTemplate", async function () {
+    const data = await irp.createPngFromPublicTemplate(
+      testTemplateId,
+      testTemplateArgs,
+      testTemplateSize
+    );
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("PNG"), true);
+  });
+
+  it("createWebpFromPrivateTemplate", async function () {
+    const data = await irp.createWebpFromPrivateTemplate(
+      testTemplateId,
+      testTemplateArgs,
+      testTemplateSize
+    );
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("WEBP"), true);
+  });
+
+  it("createWebpFromPublicTemplate", async function () {
+    const data = await irp.createWebpFromPublicTemplate(
+      testTemplateId,
+      testTemplateArgs,
+      testTemplateSize
+    );
+    assert.equal(Buffer.isBuffer(data), true);
+    assert.equal(data.includes("WEBP"), true);
   });
 });
 
